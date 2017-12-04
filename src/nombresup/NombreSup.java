3b58792e47aa370d32096f2865cfc12ab5e72a2b
Houda_Jedidi <houda-jedidi@hotmail.com>
2017-12-03 22:38:24 -0500

    Demamnde d'entre un nombre avec la verification

--- src/nombresup/NombreSup.java
+++ src/nombresup/NombreSup.java
@@ -5,17 +5,36 @@
  */
 package nombresup;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
-public class NombreSup {
+public class NombreSup
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Ce programme vous Permez d’entrer un nombre \n"+
+                "et le programme trouve le nombre supérieur suivant en utilisant seulement les chiffres du nombre donné\n"
+                + "Veuillez saisir un nombre : ");
+        int j;
+        try{
+            j=input.nextInt();
+            while(true){
+                System.out.println("Entrer "+j);
+                break; 
+            }
+            System.out.println("Bye Bye");
+        }
+        catch(InputMismatchException e){
+            System.out.println("il faut entre un nombre entier");
+        }   
     }
-    
 }
